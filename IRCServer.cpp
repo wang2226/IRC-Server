056227bc2fc737d9f8a9a97685fee23e41998507
Haoran Wang <wang2226@data.cs.purdue.edu>
2017-04-11 06:36:07 -0400

    Lab10

--- IRCServer.cpp
+++ IRCServer.cpp
@@ -520,6 +520,9 @@ IRCServer::getMessages(int fd, const char * user, const char * password, const c
 			if(inRoom != 1){
 				msg = "ERROR (User not in room)\r\n";
 			}else{
+const char *buffer[10];
+sprintf(buffer, "%d", lastMsgNum);
+write(fd, buffer, strlen(buffer));
 				map<string, vector<string> >::iterator itVec = msgInRoom.find(room); 
 				vector<string> vec = itVec->second;
 				int size = vec.size();		
